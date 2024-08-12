77d68033e6875100ad5b3e40eb143257699a569a
VamsiKrishna <appwebdev1993@gmail.com>
2024-08-12 10:25:05 +0530

    Database configure

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from "express";
+import processEnvConfig from "./app/config/processEnvConfig.js";
+import {
+  connectDB,
+  reconnectDB,
+  closeDBConnection,
+} from "./app/database/index.js";
 
 const app = express();
-
+processEnvConfig();
 app.get("/", (req, res) => res.send("Welcome"));
 
-app.listen(4000, () => {
+app.listen(4000, async () => {
   console.log("start");
+  await connectDB();
+  reconnectDB();
+  closeDBConnection();
 });
